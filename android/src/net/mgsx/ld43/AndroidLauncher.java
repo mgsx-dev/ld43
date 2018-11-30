fb501806816ec3e1c8ef0f08b6d58716ad5f9dc3
Germain AUBERT <germain.aubert@gmail.com>
2018-11-30 20:19:31 +0100

    init from template

---
+++ android/src/net/mgsx/ld43/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.mgsx.ld43;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.mgsx.ld43.LD43;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD43(), config);
+	}
+}
---
+++ desktop/src/net/mgsx/ld43/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mgsx.ld43;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mgsx.ld43.LD43;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD43(), config);
+	}
+}
