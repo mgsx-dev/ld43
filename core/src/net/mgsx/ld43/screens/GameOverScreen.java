7b058ebd2f127ee354fbfea248af6d18a84a746a
Germain AUBERT <germain.aubert@gmail.com>
2018-12-02 19:42:49 +0100

    fix screen color

--- core/src/net/mgsx/ld43/screens/GameOverScreen.java
+++ core/src/net/mgsx/ld43/screens/GameOverScreen.java
@@ -1,5 +1,8 @@
 package net.mgsx.ld43.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 
@@ -9,6 +12,8 @@ import net.mgsx.ld43.utils.StageScreen;
 
 public class GameOverScreen extends StageScreen
 {
+	private Color bgColor = new Color(Color.DARK_GRAY);
+	
 	public GameOverScreen() {
 		Table root = new Table(GameAssets.i.skin);
 		root.setFillParent(true);
@@ -25,5 +30,12 @@ public class GameOverScreen extends StageScreen
 					}
 				})
 				));
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(bgColor.r, bgColor.g, bgColor.b, 0);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		super.render(delta);
 	}
 }
