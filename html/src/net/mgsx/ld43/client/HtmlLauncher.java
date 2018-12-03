e2a904c6d5f50beed4406850e6d33804814beb82
Germain AUBERT <germain.aubert@gmail.com>
2018-12-03 21:23:24 +0100

    fix GWT viewport and logo

--- html/src/net/mgsx/ld43/client/HtmlLauncher.java
+++ html/src/net/mgsx/ld43/client/HtmlLauncher.java
@@ -1,8 +1,11 @@
 package net.mgsx.ld43.client;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.google.gwt.event.logical.shared.ResizeEvent;
+import com.google.gwt.event.logical.shared.ResizeHandler;
 
 import net.mgsx.ld43.LD43;
 
@@ -17,4 +20,28 @@ public class HtmlLauncher extends GwtApplication {
         public ApplicationListener createApplicationListener () {
                 return new LD43();
         }
+        
+        @Override
+	    public void onModuleLoad () {
+	        super.onModuleLoad();
+	        com.google.gwt.user.client.Window.addResizeHandler(new ResizeHandler() {
+	              public void onResize(ResizeEvent ev) {
+	            	  final int width = ev.getWidth();
+	            	  final int height = ev.getHeight();
+	            	  getCanvasElement().setWidth(width);
+	            	  getCanvasElement().setHeight(height);
+	            	  
+	            	  // TODO try with this to confirms there is no need for the post runnable trick
+	            	  // Gdx.graphics.setWindowedMode(width, height);
+	            	  // TODO see gwt fullscreen mode already implemented !
+	            	  
+	            	  Gdx.app.postRunnable(new Runnable() {
+						@Override
+						public void run() {
+							Gdx.app.getApplicationListener().resize(width, height);
+						}
+					});
+	              }
+	            });
+	    }
 }
